999bd8437dec51960277a093e5190b014f9dca16
Eric Kenney <ekenney5@gmail.com>
2014-05-10 20:31:35 -0700

    Decoupled movement and shooting. But laggy

--- moving_object.js
+++ moving_object.js
@@ -11,23 +11,24 @@
     var MovingObject = AST.MovingObject = function (radius, color) {
         this.pos = [0, 0];
         this.vel = [0, 0];
+        this.dir = [1, 0];
+
         this.radius = radius;
         this.color = color;
-        //I added this.
+
         this.strokeColor = color;
         this.strokeWidth = 1;
     };
 
     MovingObject.prototype.move = function () {
         this.pos = [this.pos[0] + this.vel[0],
-                    this.pos[1] + this.vel[1]];
+        this.pos[1] + this.vel[1]];
     };
 
     MovingObject.prototype.draw = function (c) {
         c.beginPath();
 
-        c.arc(this.pos[0], this.pos[1], this.radius,
-            0, 2 * Math.PI, false);
+        c.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, false);
 
         c.fillStyle = this.color;
         c.fill();
@@ -43,10 +44,11 @@
     MovingObject.prototype.isCollidedWith = function (otherObject) {
         var distance = AST.distance(this.pos, otherObject.pos);
         var collideDistance = (this.radius + this.strokeWidth +
-             otherObject.radius + otherObject.strokeWidth);
-        return (distance <= collideDistance)
-    };
+            otherObject.radius + otherObject.strokeWidth);
+            return (distance <= collideDistance)
+        };
 
+<<<<<<< HEAD
     var distance = AST.distance = function (pos1, pos2) {
         xDiff = Math.abs(pos1[0] - pos2[0]);
         yDiff = Math.abs(pos1[1] - pos2[1]);
@@ -63,3 +65,21 @@
     };
     
 })(this);
+=======
+        var distance = AST.distance = function (pos1, pos2) {
+            xDiff = Math.abs(pos1[0] - pos2[0]);
+            yDiff = Math.abs(pos1[1] - pos2[1]);
+            return Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
+        }
+    
+        var getSpeed = AST.getSpeed = function (vel) {
+            return Math.sqrt(vel[0] * vel[0] + vel[1] * vel[1])
+        };
+    
+        var normalize = AST.normalize = function (vector, normalizeTo) {
+            var speed = getSpeed(vector)
+            return [vector[0] * normalizeTo / speed, vector[1] * normalizeTo / speed];
+        };
+    
+    })(this);
+>>>>>>> Decoupled movement and shooting. But laggy
